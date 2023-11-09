3ea6629c282926e1d429cb85f026b95825e2ae95
itsDev-Developer <148090753+itsDev-Developer@users.noreply.github.com>
2023-11-09 19:17:58 +0530

    Update config.py
    Updated

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "").split(",") if i.strip()] 
+API_ID = int(os.getenv("API_ID", "23267035"))
+API_HASH = os.environ.get("API_HASH", "a516af48bca80b9f339980650126149c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6332894117:AAEitpZdPorLOBONhgF-kdnrKX3HLww3jlk")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "5729587090").split(",") if i.strip()] 
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Mongodb url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://animebot:animesdkbot@cluster0.cybneuo.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5729587090")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "App4Earn") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
 LINK_BYPASS = "False"
